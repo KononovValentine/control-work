70a13f32e0a6ee14ef87f4ae9bd1cfff2abdc76b
Valentine Kononov <Kononov.Valentine@gmail.com>
2022-11-08 22:21:20 +0500

    function SortArrayMaxCountSymbols added

--- Program/Program.cs
+++ Program/Program.cs
@@ -13,3 +13,18 @@ int ReadDataNumber(string msg)
     int number = int.Parse(System.Console.ReadLine());
     return number;
 }
+
+// Создает из заданного массива новый массив с элементами
+static (string[] array, int count) SortArrayMaxCountSymbols(string[] array, int substringNumber) {
+    int count = 0;
+    string[] sortedArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= substringNumber) 
+        {
+            sortedArray[count] = array[i];
+            count++;
+        }
+    }
+    return (sortedArray, count);
+}
