9679991f172ab6919d04662c359e97d3bd6e3477
Valentine Kononov <Kononov.Valentine@gmail.com>
2022-11-08 22:22:11 +0500

    function PrintArray added

--- Program/Program.cs
+++ Program/Program.cs
@@ -43,3 +43,14 @@ string[] CreateFinalArray(string[] array, int index)
     }
     return finalArray;
 }
+
+// Выводит массив в консоль
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1] + "]");
+}
